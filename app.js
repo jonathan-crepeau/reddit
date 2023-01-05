6f7bbf3b977d7067129bf4c319686b5db9b75715
jonathan-crepeau <jon.crepeau@gmail.com>
2023-01-04 23:27:01 -0800

    created indexed object of objects

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('app.js reporting for duty..');
+// console.log('app.js reporting for duty..');
+// NOTE - Array.isArray(value):
+
+const userArray = [
+    {id: 0, name: "Jonathan"},
+    {id: 1, name: "Jeremy"},
+    {id: 2, name: "Luna"}
+];
+
+const userMealPreferences = [
+    {user_id: 0, food: "Pork Shoulder Stew"},
+    {user_id: 1, food: "Pho"},
+    {user_id: 2, food: "Bonito Flakes"}
+];
+
+const indexedObjOfObj = (inputArray) => {
+    const indexedObject = {};
+    for (let i = 0; i <= inputArray.length - 1; i++) {
+        indexedObject[i] = inputArray[i];
+    }
+    return indexedObject;
+}
+
+const newUserObj = indexedObjOfObj(userArray);
+console.log(newUserObj);
