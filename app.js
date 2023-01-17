5da3c9bbe998d0b0bc7341b172c41db65d720ae1
jonathan-crepeau <jon.crepeau@gmail.com>
2023-01-17 11:26:58 -0800

    updated combining data sets exercise

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // console.log('app.js reporting for duty..');
 // NOTE - Array.isArray(value):
 
+// SECTION - Arrays:
+
 const usersArray = [
-    {id: 0, name: "Jonathan"},
+    {id: 7, name: "Cinnamon"},    
     {id: 1, name: "Jeremy"},
+    {id: 5, name: "Michele"},    
     {id: 2, name: "Luna"},
-    {id: 3, name: "Bruce"},
-    {id: 4, name: "Michele"},
-    {id: 5, name: "Ryan"},
+    {id: 3, name: "Ryan"},
+    {id: 0, name: "Jonathan"},
+    {id: 4, name: "Bruce"},
     {id: 6, name: "Kristen"},
-    {id: 7, name: "Cinnamon"}
 ];
 
 const foodPreferences = [
@@ -23,26 +25,51 @@ const foodPreferences = [
     {user_id: 2, food: "Tuna"}
 ]
 
-const newIndexedObj = (inputArray) => {
-    const newObject = {};
-    for (let i = 0; i <= inputArray.length - 1; i++) {
-        newObject[i] = inputArray[i];
+// NOTE sort usersArray by id integers:
+// const sortedUsersById = [...usersArray].sort((a, b) => a.id - b.id);
+// console.log(sortedUsersById);
+
+// NOTE - sort array, return as indexed object:
+const createIndexedObj = (usersArr, foodArr) => {
+    const sortedArr = [...usersArr].sort((a, b) => a.id - b.id);
+    const newObj = {};
+    for (let i = 0; i < sortedArr.length; i++) {
+        newObj[i] = sortedArr[i];
     }
-    return newObject;
+    for (let i = 0; i < foodArr.length; i++) {
+        let selectedUser = newObj[foodArr[i].user_id];
+        selectedUser.foodPref = foodArr[i].food;
+    }
+    return newObj;
 }
 
-const indexedUsersObj = newIndexedObj(usersArray);
-console.log(indexedUsersObj);
+const orderedUsers = createIndexedObj(usersArray, foodPreferences);
+console.log(orderedUsers);
 
 
-const matchPreferences = (inputArray) => {
-    for (let i = 0; i <= inputArray.length - 1; i++) {
-        let userInQuestion = indexedUsersObj[inputArray[i].user_id];
-        userInQuestion.foodPref = inputArray[i].food
-        // console.log(inputArray[i]);
-        // console.log(userInQuestion);
-    }
-}
 
-matchPreferences(foodPreferences);
-console.log(indexedUsersObj)
+// SECTION - v1
+
+// const newIndexedObj = (inputArray) => {
+//     const newObject = {};
+//     for (let i = 0; i <= inputArray.length - 1; i++) {
+//         newObject[i] = inputArray[i];
+//     }
+//     return newObject;
+// }
+
+// const indexedUsersObj = newIndexedObj(usersArray);
+// console.log(indexedUsersObj);
+
+
+// const matchPreferences = (inputArray) => {
+//     for (let i = 0; i <= inputArray.length - 1; i++) {
+//         let userInQuestion = indexedUsersObj[inputArray[i].user_id];
+//         userInQuestion.foodPref = inputArray[i].food
+//         // console.log(inputArray[i]);
+//         // console.log(userInQuestion);
+//     }
+// }
+
+// matchPreferences(foodPreferences);
+// console.log(indexedUsersObj)
